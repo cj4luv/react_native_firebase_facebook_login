a0b7139628545d8cfbb785d645f02a54b1e4e4b1
fifty bridge <cj4luv@gmail.com>
2017-01-25 15:41:59 +0900

    react-native-skeleton

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Component } from 'react-native';
+
+import { app } from './containers';
+
+const Main = () => {
+  return (
+      <app.App />
+  )
+}
+
+export default Main
