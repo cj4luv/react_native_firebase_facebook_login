5243f92e9c909e106ea8dbd08b78365ac03bcbb7
fifty bridge <cj4luv@gmail.com>
2017-02-01 19:03:52 +0900

    update:컴포넌트 화

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -9,20 +9,13 @@ import {
   Text
 } from 'react-native';
 
-const config = {
-  apiKey: "AIzaSyBBAvfR4ffQmKtg2GQTqAi3htH3R2ehvqw",
-  authDomain: "roompackersdb.firebaseapp.com",
-  databaseURL: "https://roompackersdb.firebaseio.com",
-  storageBucket: "roompackersdb.appspot.com",
-  messagingSenderId: "332389298611"
-}
 
 import LoginPage from '../loginPage/LoginPage'
 
 class App extends Component {
   render() {
     return (
-      <LoginPage config={config} />
+      <LoginPage/>
     );
   }
 }
