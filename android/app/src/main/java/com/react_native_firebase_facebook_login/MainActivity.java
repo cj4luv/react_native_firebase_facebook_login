1bc4aefed6355beebc3fbd7a2fb8002c30b2b137
fifty bridge <cj4luv@gmail.com>
2017-02-14 16:57:25 +0900

    페이스북 안드로이드 초기 설정 셋팅

--- android/app/src/main/java/com/react_native_firebase_facebook_login/MainActivity.java
+++ android/app/src/main/java/com/react_native_firebase_facebook_login/MainActivity.java
@@ -2,14 +2,45 @@ package com.react_native_firebase_facebook_login;
 
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "react_native_firebase_facebook_login";
-    }
+import java.util.Arrays;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
+import android.app.Application;
+
+public class MainActivity extends ReactActivity {
+  CallbackManager mCallbackManager =
+    MainApplication.getCallbackManager();
+
+  @Override
+  protected List<ReactPackage> getPackages() {
+    mCallbackManager = new CallbackManager.Factory().create();
+    ReactPackage packages[] = new ReactPackage[]{
+      new MainReactPackage(),
+      new FBSDKPackage(mCallbackManager),
+    };
+    return Arrays.<ReactPackage>asList(packages);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    mCallbackManager.onActivityResult(requestCode, resultCode, data);
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript.
+   * This is used to schedule rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+      return "react_native_firebase_facebook_login";
+  }
 }
